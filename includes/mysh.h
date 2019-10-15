bdc825c4a095b7a991102fd1ad0b107ecf3eca4a
Zouhair <zamazzal@student.1337.ma>
2019-10-15 15:59:41 +0100

    norm update

--- includes/mysh.h
+++ includes/mysh.h
@@ -6,7 +6,7 @@
 /*   By: zamazzal <zouhir.amazzal@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/11 19:48:06 by zamazzal          #+#    #+#             */
-/*   Updated: 2019/08/06 16:26:03 by zamazzal         ###   ########.fr       */
+/*   Updated: 2019/10/15 15:59:29 by zamazzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,19 +81,19 @@
 **		Variables
 */
 
-typedef struct s_info
+typedef struct	s_info
 {
-    int fd;
-    int mode;
-} t_info;
+	int			fd;
+	int			mode;
+}				t_info;
 
 # define ALNUM "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789"
 
-char	**g_environ;
-int		g_pid;
-int		g_input_type;
-char	*g_input;
-int		g_pipe;
+char			**g_environ;
+int				g_pid;
+int				g_input_type;
+char			*g_input;
+int				g_pipe;
 
 # define SQ 39
 # define DQ 34
@@ -106,127 +106,127 @@ int		g_pipe;
 **		srcs/shell
 */
 
-char	*ft_prepareinput(void);
-void	ft_endinput(char **cmds, char *input);
-char	**createcmds(char *input);
-void	ft_shell(void);
-char	*readline(void);
-char	*readline2(void);
-int		ft_putcmds(char **cmd);
-int		ft_checknoprint(int key, int pos);
+char			*ft_prepareinput(void);
+void			ft_endinput(char **cmds, char *input);
+char			**createcmds(char *input);
+void			ft_shell(void);
+char			*readline(void);
+char			*readline2(void);
+int				ft_putcmds(char **cmd);
+int				ft_checknoprint(int key, int pos);
 
 /*
 **		input
 **		srcs/input
 */
 
-int		checkbackslash(char *ptr, int i);
-char	*makecmdclear(char *cmd);
-char	*ft_endbackslash(char *cmd);
-char	*ft_closepipe(char *cmd);
-char	*ft_closequotes(char *cmd);
-int		ft_checkquote(char *cmd);
-int		ft_checkbackslash(char *cmd);
-int		ft_checkpipe(char *cmd);
-char	*ft_removeantin(char *str);
-int		ft_inputerror(char *input, int mode);
-int		ft_checkcmds(char **array);
-int		ft_strisprint(char *str);
+int				checkbackslash(char *ptr, int i);
+char			*makecmdclear(char *cmd);
+char			*ft_endbackslash(char *cmd);
+char			*ft_closepipe(char *cmd);
+char			*ft_closequotes(char *cmd);
+int				ft_checkquote(char *cmd);
+int				ft_checkbackslash(char *cmd);
+int				ft_checkpipe(char *cmd);
+char			*ft_removeantin(char *str);
+int				ft_inputerror(char *input, int mode);
+int				ft_checkcmds(char **array);
+int				ft_strisprint(char *str);
 
 /*
 **		Builtins
 **		srcs/builtins
 */
 
-char	*ft_getenv(const char *name);
-void	ft_setinenv(int argc, char **argv);
-void	ft_setenv(char *name, char *content);
-void	ft_echo(int argc, char **argv);
-void	ft_env(void);
-int		ft_builtins(char **argv, t_info info);
-void	ft_unsetenv(char **name);
-void	ft_cd(char **argv);
+char			*ft_getenv(const char *name);
+void			ft_setinenv(int argc, char **argv);
+void			ft_setenv(char *name, char *content);
+void			ft_echo(int argc, char **argv);
+void			ft_env(void);
+int				ft_builtins(char **argv, t_info info);
+void			ft_unsetenv(char **name);
+void			ft_cd(char **argv);
 
 /*
 **		Commands
 **		srcs/commands
 */
 
-char	**ms_parse(char *line);
-char	*ms_expand_arg(char *arg);
-void	ms_set_quote(int *quoted, char *quote, char c);
-int		ft_commands(char **argv, char *name, int fd);
-void	exec_cmd(char *exec, char **argv, int fd);
+char			**ms_parse(char *line);
+char			*ms_expand_arg(char *arg);
+void			ms_set_quote(int *quoted, char *quote, char c);
+int				ft_commands(char **argv, char *name, int fd);
+void			exec_cmd(char *exec, char **argv, int fd);
 
 /*
 **		errors
 **		srcs/errors
 */
 
-int		check_reds(char *cmd, int end);
-int		ft_redirerror(char *cmd);
-int		parseerror(char *cmd, int mode);
-void	ft_puterror(char *error);
+int				check_reds(char *cmd, int end);
+int				ft_redirerror(char *cmd);
+int				parseerror(char *cmd, int mode);
+void			ft_puterror(char *error);
 
 /*
 **		parsing
 **		srcs/parsing
 */
 
-char	**ft_cmdsplit(char *ptr, int c);
+char			**ft_cmdsplit(char *ptr, int c);
 
 /*
 **		termcaps
 **		srcs/term
 */
 
-int		ft_checktitty(void);
-int		ft_tinit(void);
-void	ft_term_prepare(int mode);
-void	ft_term_canon(void);
-void	ft_term_nocanon(void);
-void	ft_term_echo(void);
-void	ft_term_noecho(void);
+int				ft_checktitty(void);
+int				ft_tinit(void);
+void			ft_term_prepare(int mode);
+void			ft_term_canon(void);
+void			ft_term_nocanon(void);
+void			ft_term_echo(void);
+void			ft_term_noecho(void);
 
 /*
 **		signals
 **		srcs/signals
 */
 
-void	ft_signal(int x);
+void			ft_signal(int x);
 
 /*
 **		Messages
 **		srcs/messages
 */
 
-void	ft_prompt(void);
-void	ft_namerror(char *name);
-void	ft_cmderror(char *cmd);
-void	ft_permerror(char *cmd);
-void	ft_notdir(char *name);
-void	ft_putnstr(char *str, int n);
+void			ft_prompt(void);
+void			ft_namerror(char *name);
+void			ft_cmderror(char *cmd);
+void			ft_permerror(char *cmd);
+void			ft_notdir(char *name);
+void			ft_putnstr(char *str, int n);
 
 /*
 **		Tools
 **		srcs/tools
 */
 
-char	**ft_tabdup(char **array);
-void	extra_free(char *dead1, char *dead2);
-void	double_free(char *dead1, char **dead2);
-void	ft_freetable(char ***array);
-int		ft_strinstr(const char *s1, const char *s2, int len);
-int		ft_checkcinstr(char *ptr, int c);
-char	*ft_strjoin_lite(char *s1, char const *s2);
-char	*ft_strsub_lite(char *s, unsigned int start, size_t len);
-int		ft_tablen(char **array);
-int		ft_straddchr(char *str, char c);
-int		ft_straddchrinpos(char c, int pos);
-char	*ft_strappend(char **a_s1, char c, int overwrite);
-int		quote_end(char *cmd, int start);
-char	*ft_strojoin(char **s1, char *s2, int overwrite);
-void	ft_putintab(char ***a_chain, char *entry);
-int		ft_skipspaces(char *str);
+char			**ft_tabdup(char **array);
+void			extra_free(char *dead1, char *dead2);
+void			double_free(char *dead1, char **dead2);
+void			ft_freetable(char ***array);
+int				ft_strinstr(const char *s1, const char *s2, int len);
+int				ft_checkcinstr(char *ptr, int c);
+char			*ft_strjoin_lite(char *s1, char const *s2);
+char			*ft_strsub_lite(char *s, unsigned int start, size_t len);
+int				ft_tablen(char **array);
+int				ft_straddchr(char *str, char c);
+int				ft_straddchrinpos(char c, int pos);
+char			*ft_strappend(char **a_s1, char c, int overwrite);
+int				quote_end(char *cmd, int start);
+char			*ft_strojoin(char **s1, char *s2, int overwrite);
+void			ft_putintab(char ***a_chain, char *entry);
+int				ft_skipspaces(char *str);
 
 #endif
