c3ac8212e6d8d08a3708103203af9a596e28135e
Zouhair <zamazzal@student.1337.ma>
2019-11-14 17:36:52 +0100

    compiler

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 make -C libft
-gcc -Wall -Wextra -Werror main.c -I includes -I libft libft/libft.a srcs/*/* -o 21sh -ltermcap
+gcc -Wall -Wextra -Werror main.c -I . -I libft libft/libft.a srcs/*/* -o 21sh -ltermcap
